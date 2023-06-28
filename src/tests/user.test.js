8d2daf1b1d4de86c42c588c78716de8b74e224f8
GuillermoBPY <guillermo-benitez@outlook.com>
2023-06-27 20:12:52 -0400

    Actualizado Token

--- src/tests/user.test.js
+++ src/tests/user.test.js
@@ -1,15 +1,25 @@
 const supertest = require('supertest');
 const app = require('../app');
 
-const URL_BASE = '/api/v1/users';
+const BASE_URL = '/api/v1/users';
 let userId;
 let TOKEN;
 
-test('GET => URL_BASE, should return status code 200 and res.body to have length 1', async () => {
+beforeAll(async () => {
+  const user = {
+    email: 'prueba@academlo.com',
+    password: '1234',
+  };
+
+  const res = await supertest(app).post(`${BASE_URL}/login`).send(user);
+  TOKEN = res.body.token;
+});
+
+test('GET => BASE_URL, should return status code 200 and res.body to have length 1', async () => {
   const res = await supertest(app)
-    .get(URL_BASE)
+    .get(BASE_URL)
     .set('Authorization', `Bearer ${TOKEN}`);
 
   expect(res.status).toBe(200);
-  expect(res.status).toHaveLength(1);
+  expect(res.body).toHaveLength(1);
 });
